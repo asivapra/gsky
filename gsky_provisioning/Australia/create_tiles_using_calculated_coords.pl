f55704a084fb67f4784130556e3ef8bd95b61866
Arapaut Sivaprasad <avs@webgenie.com>
2019-09-04 11:10:34 +1000

    GSKY installation files on Raijin

--- gsky_provisioning/Australia/create_tiles_using_calculated_coords.pl
+++ gsky_provisioning/Australia/create_tiles_using_calculated_coords.pl
@@ -108,16 +108,20 @@ sub CreateTilesDC
 	$Y = $aus_bbox[3];
 
 	# The subdir to create the PNG files.
-#	print "$k1. $date\n";
-	$subdir = "$date/$zoom";
-	`mkdir -p $subdir`; # Tiles will be created in this Tmp dir
+	$subdir = "$date/$zoom"; # The subdir will be created in the CWD
+	if (!-d $subdir)
+	{
+		`mkdir -p $subdir`; # Tiles will be created in this dir
+	}
 	$max_jobs = 160; # Arbitrary limit: 10 per core
 	$curl_timeout = 6000; # Arbitrary limit
+
 	# Iterate through the X,Y coordinates to construct the tile's BBOX. 
 	$gz = `ls $subdir/*.gz`;
 	chop($gz);
 	my $size = -s $gz;
-#print "gz = $gz; $size\n";			
+	
+	# Skip if the tiles have already been created, tarred and gzipped
 	if (-f $gz) 
 	{
 		next; 
@@ -137,11 +141,14 @@ sub CreateTilesDC
 			$png = sprintf("%.2f_%.2f_%.2f_%.2f.png",$x, $y, $X, $Y); # Truncate the values to 2 decimals to accommodate any difference in the values sent by Terria
 			$png = "$subdir/$png";
 			my $size = -s $png;
+
+			# Skip if the tiles have already been created as *.png
 			if (-f $png && $size > 0) 
 			{
 				next; 
 			}
- 
+
+			# Send a curl request to the GSKY server on Raijin
 			$cmd = "curl -s --max-time $curl_timeout '$url" . "$x,$y,$X,$Y&width=256&height=256" . "'> $png";
 			system ("$cmd&");
 			if ($jobs >= $max_jobs)
@@ -267,10 +274,13 @@ sub SanityCheck
 {
 	$exit = 0;
 	if (!$sc_action) { print "You must specify an action. e.g. cc\n"; $exit = 1; }
-	if (!$layer) { print "You must specify a layer. e.g. landsat5_nbar_16day\n"; $exit = 1; }
-	if (!$date)  { print "You must specify a date. e.g. 2011-11-08\n"; $exit = 1; }
-	if (!$zoom)  { print "You must specify a zoom level. e.g. 200\n"; $exit = 1; }
-	if ($zoom < 5 || $zoom > 3000) { print "Zoom level must be between 5 and 3000. e.g. 200\n"; $exit = 1;}
+	if (!$layer) { print "You must specify a layer. e.g. landsat8_nbar_16day\n"; $exit = 1; }
+	if ($sc_action ne "dc")
+	{
+		if (!$date)  { print "You must specify a date. e.g. 2011-11-08\n"; $exit = 1; }
+		if (!$zoom)  { print "You must specify a zoom level. e.g. 200\n"; $exit = 1; }
+		if ($zoom < 5 || $zoom > 3000) { print "Zoom level must be between 5 and 3000. e.g. 200\n"; $exit = 1;}
+	}
 	if ($exit)
 	{
 		print "Syntax: create_tiles_all.pl cc landsat5_nbar_16day 2011-11-08 50 200\nExitting!\n";
@@ -324,7 +334,7 @@ sub do_main
 		$homedir = `pwd`;
 		chop($homedir);
 #		@zoom_levels = (3000,1000,500,300,200,100);		
-		@zoom_levels = (20);		
+		@zoom_levels = (50,20,10);		
 		#sentinel2_nbart_daily
 #		@times = ("2015-08-21","2015-10-20","2015-12-27","2016-01-06","2016-02-20","2016-04-11","2016-06-16","2016-08-04","2016-09-25","2016-10-05","2016-12-02","2016-12-08","2017-01-20","2017-03-27","2017-05-16","2017-06-30","2017-07-11","2017-08-22","2017-08-28","2017-09-03","2017-09-09","2017-10-23","2017-10-29","2017-11-02","2017-11-08","2018-02-11");
 
@@ -332,7 +342,7 @@ sub do_main
 #		@times = ("2013-03-19","2013-04-04","2013-04-20","2013-05-06","2013-05-22","2013-06-07","2013-06-23","2013-07-09","2013-07-25","2013-08-10","2013-08-26","2013-09-11","2013-09-27","2013-10-13","2013-10-29","2013-11-14","2013-11-30","2013-12-16","2014-01-01","2014-01-17","2014-02-02","2014-02-18","2014-03-06","2014-03-22","2014-04-07","2014-04-23","2014-05-09","2014-05-25","2014-06-10","2014-06-26","2014-07-12","2014-07-28","2014-08-13","2014-08-29","2014-09-14","2014-09-30","2014-10-16","2014-11-01","2014-11-17","2014-12-03","2014-12-19","2015-01-04","2015-01-20","2015-02-05","2015-02-21","2015-03-09","2015-03-25","2015-04-10","2015-04-26","2015-05-12","2015-05-28","2015-06-13","2015-06-29","2015-07-15","2015-07-31","2015-08-16","2015-09-01","2015-09-17","2015-10-03","2015-10-19","2015-11-04","2015-11-20","2015-12-06","2015-12-22","2016-01-07","2016-01-23","2016-02-08","2016-02-24","2016-03-11","2016-03-27","2016-04-12","2016-04-28","2016-05-14","2016-05-30","2016-06-15","2016-07-01","2016-07-17","2016-08-02","2016-08-18","2016-09-03","2016-09-19","2016-10-05","2016-10-21","2016-11-06","2016-11-22","2016-12-08","2016-12-24","2017-01-09","2017-01-25","2017-02-10","2017-02-26","2017-03-14","2017-03-30","2017-04-15","2017-05-01","2017-05-17","2017-06-02","2017-06-18","2017-07-04","2017-07-20","2017-08-05","2017-08-21","2017-09-06","2017-09-22","2017-10-08","2017-10-24","2017-11-09","2017-11-25","2017-12-11","2017-12-27","2018-01-12","2018-01-28","2018-02-13","2018-03-01","2018-03-17","2018-04-02","2018-04-18","2018-05-04","2018-05-20","2018-06-05","2018-06-21","2018-07-07","2018-07-23","2018-08-08","2018-08-24","2018-09-09","2018-09-25","2018-10-11","2018-10-27","2018-11-12","2018-11-28","2018-12-14","2018-12-30","2019-01-15","2019-01-31","2019-02-16","2019-03-04","2019-03-20","2019-04-05","2019-04-21","2019-05-07");
 #		@times = ("2013-04-20","2013-05-06","2013-05-22","2013-06-07","2013-06-23");
 #		@times = ("2013-05-06","2013-05-22");
-		@times = ("2013-03-19");
+		@times = ("2013-04-20","2013-05-06","2013-05-22","2013-06-07","2013-06-23");
 
 			my $lt = $#times;
 			$n_times = $lt+=1;
@@ -350,8 +360,8 @@ sub do_main
 					$date = $time;
 					$date =~ s/T00:00:00.000Z//g;
 					$url = "http://localhost:9511/ows?time=$time&srs=EPSG:3857&transparent=true&format=image/png&exceptions=application/vnd.ogc.se_xml&styles=&tiled=true&feature_count=101&service=WMS&version=1.1.1&request=GetMap&layers=$layer&bbox=";
-#					print "$k. $date; $zoom\n";
 					&CreateTilesDC;
+					&CreateTilesDC; # Run this twice so that those skipped tiles (due to RPC errors) will be re-created. Tiles that are already there will not be fetched again.
 					&WaitForJobs;
 					&PurgeBlanksAndCreateTarGz;
 					$ct1 = time();
